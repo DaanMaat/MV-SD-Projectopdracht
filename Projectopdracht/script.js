8f6999e18500ddfc3632611e298f9749eaabd255
Daan Maat <dmaat@roc-dev.com>
2022-01-26 14:31:14 +0100

    iets dat EINDELIJK is gefixed

---
+++ Projectopdracht/script.js
@@ -0,0 +1,12 @@
+var telButton = 0;
+
+document.getElementsByClassName("button")[0].onclick = function () {
+    telButton++;
+    if (telButton == 1) {
+        document.getElementById("navigatie-containerButton").style.marginLeft = "3vw"
+    } else if (telButton == 2) {
+        document.getElementById("navigatie-containerButton").style.marginLeft = "-21vw"
+        telButton = 0;
+    }
+
+}
