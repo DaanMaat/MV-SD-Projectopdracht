bf0395611b0fa74dc0d33cfcd03c3ed3fb81b2ad
SamMayer0815 <sammayer0815@gmail.com>
2022-01-27 14:05:01 +0100

    responsive update
    responsive update

--- Projectopdracht/script.js
+++ Projectopdracht/script.js
@@ -3,9 +3,9 @@ var telButton = 0;
 document.getElementsByClassName("button")[0].onclick = function () {
     telButton++;
     if (telButton == 1) {
-        document.getElementById("navigatie-containerButton").style.marginLeft = "3vw"
+        document.getElementById("navigatie-containerButton").style.marginLeft = "50vw"
     } else if (telButton == 2) {
-        document.getElementById("navigatie-containerButton").style.marginLeft = "-21vw"
+        document.getElementById("navigatie-containerButton").style.marginLeft = "-50vw"
         telButton = 0;
     }
 
