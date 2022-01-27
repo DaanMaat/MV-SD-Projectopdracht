dc1767a19515a8bdf029ec2533825f7312cd0f06
SamMayer0815 <sammayer0815@gmail.com>
2022-01-27 16:31:18 +0100

    laatste fix
    laatste fix

--- Projectopdracht/script.js
+++ Projectopdracht/script.js
@@ -10,3 +10,51 @@ document.getElementsByClassName("button")[0].onclick = function () {
     }
 
 }
+
+document.addEventListener("mouseover", function(event){
+    
+    if(event.target.id == "about" || "aboutLink"){
+        document.getElementById("aboutLink").style.color = "black"
+    }
+    if(event.target.id != "about" && event.target.id != "aboutLink"){
+        document.getElementById("aboutLink").style.color = "rgb(140,183,127)"
+    }
+
+    if(event.target.id == "opleidingen" || "opledingenLink"){
+        document.getElementById("opleidingenLink").style.color = "black"
+    }
+    if(event.target.id != "opleidingen" && event.target.id != "opleidingenLink"){
+        document.getElementById("opleidingenLink").style.color = "rgb(140,183,127)"
+    }
+
+    if(event.target.id == "projecten"  || "projectenLink"){
+        document.getElementById("projectenLink").style.color = "black"
+    }
+    if(event.target.id != "projecten" && event.target.id != "projectenLink"){
+        document.getElementById("projectenLink").style.color = "rgb(140,183,127)"
+    }
+
+    if(event.target.id == "skills" || "skillsLink"){
+        document.getElementById("skillsLink").style.color = "black"
+    }
+    if(event.target.id != "skills" && event.target.id != "skillsLink"){
+        document.getElementById("skillsLink").style.color = "rgb(140,183,127)"
+    }
+
+    if(event.target.id == "werkervaringen" || "werkervaringLink"){
+        document.getElementById("werkervaringLink").style.color = "black"
+    }
+    if(event.target.id != "werkervaringen" && event.target.id != "werkervaringLink"){
+        document.getElementById("werkervaringLink").style.color = "rgb(140,183,127)"
+    }
+
+    if(event.target.id == "contact" || "contactLink"){
+        document.getElementById("contactLink").style.color = "black"
+    }
+    if(event.target.id != "contact" && event.target.id != "contactLink"){
+        document.getElementById("contactLink").style.color = "rgb(140,183,127)"
+    }
+
+
+
+})
